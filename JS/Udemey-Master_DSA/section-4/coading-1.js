5ea8749e849d2b99a0b8cc212841475bf03f5e9f
Yogendra Singh <sonu831@gmail.com>
2025-01-14 16:04:54 +0530

    master DSA

---
+++ JS/Udemey-Master_DSA/section-4/coading-1.js
@@ -0,0 +1,25 @@
+//Check if we have common in array
+
+const arr1 = ["a", "b", "e", "c"];
+
+const arr2 = ["c", "z", "d", "j"];
+
+function checkCommonItem(arr1, arr2) {
+  const obj = {};
+  for (let i = 0; i < arr1.length; i++) {
+    if (!obj[arr1[i]]) {
+      obj[arr1[i]] = true;
+    }
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    const item = arr2[i];
+    if (obj[item]) {
+        console.log("common-item",item)
+        return true;
+    }
+  }
+  return false;
+}
+
+console.log(checkCommonItem(arr1, arr2));
